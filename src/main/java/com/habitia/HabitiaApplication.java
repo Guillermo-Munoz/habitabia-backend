da61f3c2c01b676663f26a6906a2e372aeb64bb6
Guillermo-Munoz <illloquehaces@hotmail.com>
2026-03-31 20:51:10 +0200

    chore: update main application class

--- src/main/java/com/habitia/HabitiaApplication.java
+++ src/main/java/com/habitia/HabitiaApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import com.habitia.shared.infrastructure.security.JwtProperties;
+import com.habitia.shared.infrastructure.storage.StorageProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)  // ← IMPORTANTE!
+@EnableConfigurationProperties({JwtProperties.class, StorageProperties.class})
 public class HabitiaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HabitiaApplication.class, args);
